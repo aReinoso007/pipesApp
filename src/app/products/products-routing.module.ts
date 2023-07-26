d37cdc5689f711fb66ccf249b39a6236a9efa57e
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-26 14:01:04 -0500

    pages componnts

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductsComponent } from './products.component';
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  {
+    path:'',
+    component: BasicsPageComponent
+  },
+  {
+    path:'Uncommon',
+    component: UncommonPageComponent
+  },
+  {
+    path:'Numbers',
+    component: NumbersPageComponent
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
