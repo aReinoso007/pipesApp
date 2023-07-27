bba39a9449bf0d86c8245ecf3863426580aa64c8
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-26 23:30:55 -0500

    pipes

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'alex';
+  public nameUpper: string = 'ALEX';
+  public fullName: string = 'aLEx rEinOSO';
+
 }
