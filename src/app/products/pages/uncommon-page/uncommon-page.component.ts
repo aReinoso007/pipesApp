b6ae2317771aa384fccb3a9eec6989a27ee80533
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-08-17 23:41:32 -0500

    i18nSelect pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18nSelect
+  public name: String = 'Alex';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap ={
+    'male':'invitarlo',
+    'female':'invitarla'
+  }
+  changeClient(): void {
+    this.name ="Sharon"
+    this.gender = 'female';
+  }
 }
